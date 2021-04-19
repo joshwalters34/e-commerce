f3fb9dd86312eeecece402f6f8e933c02de452db
JWalters <joshwalters34@gmail.com>
2021-04-18 21:57:32 -0400

    inital commit.  Also added code to connect server and finished creating models

---
+++ Develop/models/index.js
@@ -0,0 +1,35 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'products',
+  foreignKey: "product_id",
+});
+// Products belongToMany Tags (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'tag_id',
+});
+// Tags belongToMany Products (through ProductTag)
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
